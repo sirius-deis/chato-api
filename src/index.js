2b73f207f02694e2ac95f38bdd030697fbc5e909
Sirius Deis <falebsy@gmail.com>
2023-05-21 00:22:25 +0200

    add handlers for process errors and shutdown events

--- src/index.js
+++ src/index.js
@@ -6,18 +6,69 @@ const { PORT = 3000 } = process.env;
 
 const server = app.listen(PORT, () => {
     console.log(
-        chalk.green.bold('SERVER STATUS: '),
-        chalk.green(`Server us running on port: ${PORT}`)
+        chalk.bgGreen.bold('SERVER STATUS: '),
+        chalk.whiteBright('Server is running on port:'),
+        chalk.green.bold(`${PORT}`)
     );
 });
 
 process.on('unhandledRejection', err => {
     console.log(
-        chalk.red.bold('SERVER STATUS'),
-        chalk.red(`Unhandled rejection!`),
-        chalk.red.italic(err.name, err.message)
+        chalk.bgRed.bold('SERVER STATUS'),
+        chalk.redBright(`UNHANDLED REJECTION!`),
+        chalk.redBright.italic(err?.name, err?.stack)
     );
     server.close(() => {
         process.exit(1);
+    });
+});
+
+process.on('uncaughtException', err => {
+    console.log(
+        chalk.bgRed.bold('SERVER STATUS'),
+        chalk.redBright(`UNCAUGHT EXCEPTION! \n`),
+        chalk.redBright.italic(err.stack)
+    );
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
+process.on('SIGTERM', () => {
+    console.log(
+        chalk.yellowBright.bold('SERVER STATUS'),
+        chalk.whiteBright(`SIGTERM RECEIVED!`)
+    );
+    server.close(() => {
+        console.log(
+            chalk.bgGreen.bold('SERVER STATUS'),
+            chalk.greenBright('Process terminated')
+        );
+    });
+});
+
+process.on('SIGINT', () => {
+    console.log(
+        chalk.yellowBright.bold('SERVER STATUS'),
+        chalk.yellow(`SIGINT RECEIVED!`)
+    );
+    server.close(() => {
+        console.log(
+            chalk.bgGreen.bold('SERVER STATUS'),
+            chalk.greenBright('Process terminated')
+        );
+    });
+});
+
+process.on('SIGQUIT', () => {
+    console.log(
+        chalk.yellowBright.bold('SERVER STATUS'),
+        chalk.yellow(`SIGQUIT RECEIVED!`)
+    );
+    server.close(() => {
+        console.log(
+            chalk.bgGreen.bold('SERVER STATUS'),
+            chalk.greenBright('Process terminated')
+        );
     });
 });
