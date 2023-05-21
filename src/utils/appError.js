1adceeeaf49d6dda6b306090272d10a015e2c286
Sirius Deis <falebsy@gmail.com>
2023-05-21 11:37:35 +0200

    add catchAsync fanction and app error constructor

---
+++ src/utils/appError.js
@@ -0,0 +1,18 @@
+const AppError = function (message, code) {
+    Error.prototype.constructor.call(this, message);
+    this.message = message;
+    this.statusCode = code;
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
+};
+
+AppError.prototype = Object.create(Error.prototype, {
+    constructor: {
+        value: AppError,
+        enumerable: false,
+        configurable: true,
+        writable: true,
+    },
+});
+
+module.exports = AppError;
