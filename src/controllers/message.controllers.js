9a73762dc05413d54dfe447613640dfdb33090f6
Sirius Deis <falebsy@gmail.com>
2023-06-06 04:41:33 +0200

    add a route for getting a single message

--- src/controllers/message.controllers.js
+++ src/controllers/message.controllers.js
@@ -4,6 +4,18 @@ const Message = require('../models/message.models');
 const DeleteMessage = require('../models/deletedMessage.models');
 const { Sequelize } = require('../db/db.config');
 
+const filterDeletedMessages = async (userId, ...messages) => {
+  const deletedMessages = await DeleteMessage.findAll({
+    where: Sequelize.and({
+      user_id: userId,
+    }),
+  });
+
+  const deletedIds = deletedMessages.map((message) => message.dataValues.message_id);
+
+  return messages.filter((message) => deletedIds.includes(message.dataValues.id));
+};
+
 exports.getMessages = catchAsync(async (req, res, next) => {
   const { user } = req;
   const { conversationId } = req.params;
@@ -19,15 +31,7 @@ exports.getMessages = catchAsync(async (req, res, next) => {
     ),
   });
 
-  const deletedMessages = await DeleteMessage.findAll({
-    where: Sequelize.and({
-      user_id: user.dataValues.id,
-    }),
-  });
-
-  const deletedIds = deletedMessages.map((message) => message.dataValues.message_id);
-
-  const messagesWithoutDeleted = messages.filter((message) => deletedIds.includes(message.dataValues.id));
+  const messagesWithoutDeleted = filterDeletedMessages(user.dataValues.id, ...messages);
 
   if (!messagesWithoutDeleted.length) {
     return next(new AppError('There are no messages for such conversation', 404));
@@ -43,9 +47,30 @@ exports.getMessages = catchAsync(async (req, res, next) => {
 
 exports.getMessage = catchAsync(async (req, res, next) => {
   const { user } = req;
-  const { conversationId } = req.params;
+  const { messageId } = req.params;
 
-  res.status(200).json({ message: 'Your messages was retrieved successfully' });
+  const message = await Message.findByPk(messageId);
+
+  if (!message) {
+    return next(new AppError('There is no message with such id', 404));
+  }
+
+  if (user.dataValues.id !== message.dataValues.sender_id) {
+    return next(new AppError('This message is not your', 403));
+  }
+
+  const messagesWithoutDeleted = filterDeletedMessages(user.dataValues.id, message);
+
+  if (!messagesWithoutDeleted[0]) {
+    return next(new AppError('There is no message with such id', 404));
+  }
+
+  res.status(200).json({
+    message: 'Your messages was retrieved successfully',
+    data: {
+      message: messagesWithoutDeleted[0],
+    },
+  });
 });
 
 exports.addMessage = catchAsync(async (req, res, next) => {
